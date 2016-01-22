97b8e3d37e17dfb0014e71d2abae3058b39dab59
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2016-01-22 08:09:24 +0000

    Add update user

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -4,11 +4,11 @@ import qualified App
 import Web.Scotty
 import qualified Data.Text as Text
 import qualified Helpers.Config as Config
-
-
+import qualified Database.UserManager as UM
 
 main = do
   envPort <- Config.getConfig "PORT"
+  index <- UM.createIndex
   scotty (read $ Text.unpack envPort :: Int) $ do
     App.app
 
--- server/test/Helpers/DatabaseTest.hs
+++ server/test/Helpers/DatabaseTest.hs
@@ -7,19 +7,20 @@ import qualified Helpers.Config as Config
 import Helpers.Database
 import Database.MongoDB
 import Models.DatabaseUser
+import qualified Database.UserManager as UM
 
 withCleanDatabase :: ActionWith () -> IO ()
 withCleanDatabase action = do 
     Config.getConfig "MONGO_DB" >>= \dbName ->
       let dropDB = performDBAction $ dropDatabase dbName
-      in dropDB >> action () >> dropDB >> return ()
+      in dropDB >> UM.createIndex >> action () >> dropDB >> UM.createIndex >> return ()
 
 defaultPassword = "somepassword"
 
 withUser :: DatabaseUser -> IO ()
 withUser user = do 
   performDBAction (insert "users" [
-      "_id" =: (email user), 
+      "email" =: (email user), 
       "confirmed" =: (confirmed user),
       "emailConfirmationToken" =: (emailConfirmationToken user)])
   return ()
